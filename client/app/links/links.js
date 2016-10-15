a894a5a204f30eebb43a52f37b8a8fdaa5d42e39
englundn <englundn@gmail.com>
2016-10-15 12:04:44 -0700

    Used custom directive to display links.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -3,4 +3,19 @@ angular.module('shortly.links', [])
 .controller('LinksController', function ($scope, links) {
   $scope.data = {};
   $scope.data.links = links;
+})
+
+.directive('shortenedLink', function() {
+  return {
+    template:
+      '<img src="../../assets/redirect_icon.png"/>' + 
+      '<span class="info">' + 
+        '<a href={{link.code}} class="title">{{link.title}}</a>' +
+        '<span class="original">{{link.url}}</span>' + 
+        '<span class="visits">' + 
+          '<span class="count">{{link.visits}}</span>' +
+          'Visits' +
+        '</span>' +
+      '</span>'
+  };
 });
