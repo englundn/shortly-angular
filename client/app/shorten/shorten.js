2b5930fab108164b3ab7bfb8dcf5ced7cedb94e3
englundn <englundn@gmail.com>
2016-10-15 11:26:27 -0700

    Refined validation for links.

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,15 +2,14 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
-  $scope.addLink = function() {
-    Links.addOne($scope.link).then(function() {
+  $scope.addLink = function(bool) {
+    bool && Links.addOne($scope.link).then(function() {
       $scope.link.url = '';
     });
   };
   $scope.checkError = function(bool) {
     if (bool) {
       $scope.class = 'clean';
-      $scope.addLink();
     } else {
       $scope.class = 'error';
     }
